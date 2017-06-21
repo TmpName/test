30d7f7f37721136ed087cea6f793181bdb086ae4
johngf <john59650@live.fr>
2017-06-21 17:11:59 +0200

    Update gui.py

--- test/gui.py
+++ test/gui.py
@@ -1,8 +1,8 @@
 #-*- coding: utf-8 -*-
 #https://github.com/Kodi-vStream/venom-xbmc-addons
 #nom du site ,fonction , nom , thumb , fanart, le reste
-#beaucoup plus simple d'avoir des dossiers spécifique
-from resources.test.util import VSlog,VStranslatePathAddon,VSlang
+
+from resources.test.util import VSlog,VStranslatePathAddon,VSlang,isKrypton
 from resources.lib.handler.inputParameterHandler import cInputParameterHandler
 import xbmc
 import xbmcgui
@@ -16,10 +16,34 @@ Pluginurl = sys.argv[0]
 Pluginhandle = int(sys.argv[1])
 
 class cGui():
+    
+    #affiche les dossiers simple par liste complete - menu context de base ou context de l'historique
+    #multi site - multi fonction etc...
+    def HomeFolder(self, Olist, History=False):
+    
+        Context = self.defaultContext()
+        Listing = []
+        for aEntry in Olist:
 
-    #utile quand liste de > un site une function un fanart - menu context de base - sous menus et toutes les sources
-    def OnesiteOnefunction(self, Site, Function, Fanart, olist): #olist >> nom, thumb , siteUrl ce qui change
+            list_item = xbmcgui.ListItem(label=aEntry[2])
 
+            list_item.setArt({'thumb': path+aEntry[3],'icon': path+aEntry[3],'fanart': path+aEntry[4]})
+            
+            if (History == False):
+                list_item.addContextMenuItems(Context)
+            else:
+                list_item.addContextMenuItems([(VSlang(30412), 'XBMC.RunPlugin('+Pluginurl+'?site=cHome&function=delSearch&searchtext='+aEntry[2]+')')])
+                
+            Url = self.CreateUrl(aEntry[0], aEntry[1], aEntry[2], aEntry[3], aEntry[4], aEntry[5])
+
+            Listing.append((Url, list_item, True))
+
+        xbmcplugin.addDirectoryItems(Pluginhandle, Listing, len(Listing))
+        
+    #sous menu home uniquement (evite repetion de texte dans home)
+    def HomeSubFolder(self, Site, Function, Fanart, olist):
+    
+        Context = self.defaultContext()
         listing = []
         for aEntry in olist:
 
@@ -27,58 +51,23 @@ class cGui():
 
             list_item.setArt({'thumb': path+aEntry[1],'icon': path+aEntry[1],'fanart': path+Fanart})
             
-            list_item.addContextMenuItems(self.defaultContext())
+            list_item.addContextMenuItems(Context)
 
-            url = self.CreateUrl(Site, Function, aEntry[0], aEntry[1], Fanart, aEntry[2])
-
-            listing.append((url, list_item, True))
-
-        xbmcplugin.addDirectoryItems(Pluginhandle, listing, len(listing))
-        
-    #affiche les dossiers simple par liste complete - menu context de base 
-    #multi site - multi fonction etc...
-    def addSimpleFolder(self, olist):
-
-        listing = []
-        for aEntry in olist:
-
-            list_item = xbmcgui.ListItem(label=aEntry[2])
-
-            list_item.setArt({'thumb': path+aEntry[3],'icon': path+aEntry[3],'fanart': path+aEntry[4]})
-            
-            list_item.addContextMenuItems(self.defaultContext())
-            
-            url = self.CreateUrl(aEntry[0], aEntry[1], aEntry[2], aEntry[3], aEntry[4], aEntry[5])
+            url = self.CreateUrl(Site, Function, aEntry[0], aEntry[1], Fanart, 'siteUrl='+aEntry[2])
 
             listing.append((url, list_item, True))
 
         xbmcplugin.addDirectoryItems(Pluginhandle, listing, len(listing))
         
-    #affiche les dossiers historique de recherche
-    #menu context spécifique pour chaque entrée
-    def addHistoryFolder(self, olist):
-    
-        listing = []
-        for aEntry in olist:
-
-            list_item = xbmcgui.ListItem(label=aEntry[2])
-
-            list_item.setArt({'thumb': path+aEntry[3],'icon': path+aEntry[3],'fanart': path+aEntry[4]})
-            
-            list_item.addContextMenuItems([(VSlang(30412), 'XBMC.RunPlugin('+Pluginurl+'?site=cHome&function=delSearch&searchtext='+aEntry[2]+')')])
-
-            url = self.CreateUrl(aEntry[0], aEntry[1], aEntry[2], aEntry[3], aEntry[4], aEntry[5])
-
-            listing.append((url, list_item, True))
-
-        xbmcplugin.addDirectoryItems(Pluginhandle, listing, len(listing))
-
-
     def defaultContext(self):
-        context = []
-        context.append((VSlang(30023), 'XBMC.RunPlugin('+Pluginurl+'?site=globalParametre&function=opensetting)'))
-        context.append((VSlang(30210), 'XBMC.Container.Update('+Pluginurl+'?site=cFav&function=getFavourites)'))
-        return context
+        Context = []
+        if (isKrypton() == True):
+            Context.append((VSlang(30023), 'XBMC.RunPlugin('+Pluginurl+'?site=globalParametre&function=opensetting)'))
+            Context.append((VSlang(30210), 'XBMC.Container.Update('+Pluginurl+'?site=cFav&function=getFavourites)'))
+        else:
+            Context.append((VSlang(30210), 'XBMC.Container.Update('+Pluginurl+'?site=cFav&function=getFavourites)'))
+            
+        return Context
         
     def CreateUrl(self,Site,Function,Title,Thumb,Fanart,Siteurl):
         #retourne l'url final
