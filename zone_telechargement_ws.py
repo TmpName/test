9293f8dc083b1c42c1c3b122d8dac0d6be302ab4
johngf <john59650@live.fr>
2018-08-07 21:42:03 +0200

    Update zone_telechargement_ws.py

--- zone_telechargement_ws.py
+++ zone_telechargement_ws.py
@@ -1,6 +1,6 @@
 #-*- coding: utf-8 -*-
 #Vstream https://github.com/Kodi-vStream/venom-xbmc-addons
-from resources.lib.gui.hoster import cHosterGui
+from resources.lib.gui.hoster import cHosterGu
 from resources.lib.gui.gui import cGui
 from resources.lib.handler.inputParameterHandler import cInputParameterHandler
 from resources.lib.handler.outputParameterHandler import cOutputParameterHandler
